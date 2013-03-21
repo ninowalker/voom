816c9f997f79e43292936561ae1647874f46d2f7
Nino Walker <nino@livefyre.com>
2013-03-20 19:47:39 -0700

    Better tests for IMAPConsumer

--- tests/imap/test_consumer.py
+++ tests/imap/test_consumer.py
@@ -11,9 +11,40 @@ from voom.imap.events import IMAPMessageDownloaded
 import os
 from unittest.case import SkipTest
 import warnings
+from mock import Mock
+import imaplib
 
 
 class Test(unittest.TestCase):
+    def test_mock_read(self):
+        conn = Mock(spec=imaplib.IMAP4_SSL)
+        imap = IMAPConsumer(("imap.gmail.com",993),
+                            "username",
+                            'password',
+                            VoomBus(),
+                            on_complete=self._on_complete,
+                            connection_type=Mock(return_value=conn))
+        
+        bus = imap.bus
+        msgs = []
+        bus.subscribe(IMAPMessageDownloaded, msgs.append)
+        bus.subscribe(IMAPMessageDownloaded, self._print, priority=BusPriority.LOW_PRIORITY**3)
+
+        with open(os.path.join(os.path.dirname(__file__), 'msg.txt')) as f:
+            mime_text = f.read()
+
+        conn.search.return_value = ['OK', ['1 2']]
+        conn.fetch.return_value = ['OK', [(None, mime_text)]]
+        conn.store.return_value = ['OK', None]
+        conn.close.side_effect = Exception
+        imap.run()
+        
+        assert len(msgs) == 2
+        m = msgs[0].mime_message
+        expected = "Ned Freed <ned@innosoft.com>"
+        assert m['To'].strip() == expected, "'%s' '%s'" % (m['To'], expected)
+        
+    
     def test_read_from_gmail(self):
         username = os.environ.get('GMAIL_USER')
         password = os.environ.get('GMAIL_PASS')
