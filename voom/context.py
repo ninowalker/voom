43ab52ef21fd0ac8678b398a9f9f51680b505e9e
Jonathan Klaassen <jonathan@livefyre.com>
2013-03-27 11:46:12 -0700

    Let users specify a reply context for out-of-band replies.

--- voom/context.py
+++ voom/context.py
@@ -6,6 +6,7 @@ MessageEnvelope = namedtuple("MessageEnvelope", ["body"])
 """A container for details about a message + failed handler"""
 InvocationFailure = namedtuple("InvocationFailure", ["message", "exception", "stack_trace", "invocation_context"])
 
+ReplyContext = namedtuple("ReplyContext", ["reply_to", "responder", "thread_channel"])
 
 class BusState(object):
     """A thread local state object."""
@@ -19,22 +20,22 @@ class BusState(object):
         """A destructive iterator for consuming all queued messages."""
         while self._queued_messages:
             yield self._queued_messages.pop()
-        
+
     def is_queue_empty(self):
         """Got messages?"""
         return len(self._queued_messages) == 0
-                
+
     def enqueue(self, message):
         """Enqueue a message during this session."""
         self._queued_messages.append(message)
-        
-            
+
+
 class Session(dict):
     """A bag for storing session variables during a Bus.publish() call."""
     def __init__(self, **kwargs):
         self.update(kwargs)
 
-        
+
 class SessionKeys(object):
 
     GATEWAY_EVENT = '_gateway_event'
